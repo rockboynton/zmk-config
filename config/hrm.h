415020921f3df18572f5c386ac7168f1da324ac4
Rock Boynton <rboynton@anduril.com>
2023-12-17 00:17:50 -0800

    add homerow mods

---
+++ config/hrm.h
@@ -0,0 +1,25 @@
+/**
+ * Homerow mods
+ */
+
+#pragma once
+
+#define ZMK_BEHAVIOR_CORE_hold_tap                                             \
+  compatible = "zmk,behavior-hold-tap";                                        \
+  #binding-cells = <2>
+
+#define MAKE_HRM(name, TRIGGER_POS)                                          \
+  / {                                                                          \
+        behaviors {                                                                \
+            name: name {                                                                   \
+                ZMK_BEHAVIOR_CORE_hold_tap;                                            \
+                flavor = "balanced";                                                   \
+                tapping-term-ms = <280>; \
+                quick-tap-ms = <175>; \
+                require-prior-idle-ms = <150>; \
+                bindings = <&kp>, <&kp>; \
+                hold-trigger-key-positions = <TRIGGER_POS>; \
+                hold-trigger-on-release; \
+            }; \
+        }; \
+    };
