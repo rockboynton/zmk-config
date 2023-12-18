d9f38a14b0e269ccab818df37764a8cb678d9180
Rock Boynton <rboynton@anduril.com>
2023-12-17 21:25:59 -0800

    fix keymap for drawer

--- config/hrm.h
+++ config/hrm.h
@@ -13,8 +13,8 @@
         behaviors {                                                                \
             name: name {                                                                   \
                 ZMK_BEHAVIOR_CORE_hold_tap;                                            \
-                flavor = "balanced";                                                   \
-                tapping-term-ms = <250>; \
+                flavor = "tap-unless-interrupted";                                                   \
+                tapping-term-ms = <100>; \
                 quick-tap-ms = <175>; \
                 require-prior-idle-ms = <150>; \
                 bindings = <&kp>, <&kp>; \
