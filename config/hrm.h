cf7bf5ba17a08f79de6ec399cc8cef2fe08840a2
Rock Boynton <rboynton@anduril.com>
2023-12-17 21:57:20 -0800

    tweaks to hrm

--- config/hrm.h
+++ config/hrm.h
@@ -14,7 +14,7 @@
             name: name {                                                                   \
                 ZMK_BEHAVIOR_CORE_hold_tap;                                            \
                 flavor = "balanced";                                                   \
-                tapping-term-ms = <100>; \
+                tapping-term-ms = <225>; \
                 quick-tap-ms = <175>; \
                 require-prior-idle-ms = <150>; \
                 bindings = <&kp>, <&kp>; \
